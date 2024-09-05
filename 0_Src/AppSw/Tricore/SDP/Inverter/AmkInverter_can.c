818b2354c2e930276e2d8b696c350976bca078b5
박상현 <sanghyean1@hanyang.ac.kr>
2024-09-06 00:06:51 +0900

    Rollback: logging inverter set

--- 0_Src/AppSw/Tricore/SDP/Inverter/AmkInverter_can.c
+++ 0_Src/AppSw/Tricore/SDP/Inverter/AmkInverter_can.c
@@ -19,10 +19,10 @@ CanCommunication_Message T_TC275_RR;
 CanCommunication_Message T_TC275_FR;
 CanCommunication_Message T_InvCtr;
 
-CanCommunication_Message T_INV_FL_AMK_Set_Point_log;
-CanCommunication_Message T_INV_RL_AMK_Set_Point_log;
-CanCommunication_Message T_INV_RR_AMK_Set_Point_log;
-CanCommunication_Message T_INV_FR_AMK_Set_Point_log;
+// CanCommunication_Message T_INV_FL_AMK_Set_Point_log;
+// CanCommunication_Message T_INV_RL_AMK_Set_Point_log;
+// CanCommunication_Message T_INV_RR_AMK_Set_Point_log;
+// CanCommunication_Message T_INV_FR_AMK_Set_Point_log;
 CanCommunication_Message T_INV_FL_AMK_Actual_Values1_log;
 CanCommunication_Message T_INV_RL_AMK_Actual_Values1_log;
 CanCommunication_Message T_INV_RR_AMK_Actual_Values1_log;
@@ -321,8 +321,8 @@ void AmkInverter_writeMessage(uint16 Value1, uint16 Value2)
     AmkInverter_can_write(&INV_FR_AMK_Setpoint1,T_TC275_FR,Value2);
 
 	// Logging
-	AmkInverter_can_write(&INV_FL_AMK_Setpoint1,T_INV_FL_AMK_Set_Point_log,Value1);
-    AmkInverter_can_write(&INV_FR_AMK_Setpoint1,T_INV_FR_AMK_Set_Point_log,Value2);
+	// AmkInverter_can_write(&INV_FL_AMK_Setpoint1,T_INV_FL_AMK_Set_Point_log,Value1);
+    // AmkInverter_can_write(&INV_FR_AMK_Setpoint1,T_INV_FR_AMK_Set_Point_log,Value2);
     // if (Inv_switch_msg.B.BE1on||Inv_switch_msg.B.BE2on||Inv_switch_msg.B.EFon){
     if(alreadyOn != 0){
         InverterControlSet();
@@ -337,8 +337,8 @@ void AmkInverter_writeMessage2(uint16 Value1, uint16 Value2)
     AmkInverter_can_write(&INV_RL_AMK_Setpoint1,T_TC275_RL,Value2);
 
 	// Logging
-	AmkInverter_can_write(&INV_FL_AMK_Setpoint1,T_INV_RR_AMK_Set_Point_log,Value1);
-    AmkInverter_can_write(&INV_FR_AMK_Setpoint1,T_INV_RL_AMK_Set_Point_log,Value2);
+	// AmkInverter_can_write(&INV_FL_AMK_Setpoint1,T_INV_RR_AMK_Set_Point_log,Value1);
+    // AmkInverter_can_write(&INV_FR_AMK_Setpoint1,T_INV_RL_AMK_Set_Point_log,Value2);
 }
 
 void AmkInverter_Start(boolean rtdFlag)
@@ -592,10 +592,10 @@ static void setTransmitMessageLog(uint32_t ID, CanCommunication_Message *Tm,uint
 void AMKInverter_initLoggingMessage(void) {
 
 	//Init CAN msg of AMK_Set_Point
-	setTransmitMessageLog((uint32_t)Inverter_FL.ID_AMK_Set + (uint32_t)0x275000, &T_INV_FL_AMK_Set_Point_log,0);
-	setTransmitMessageLog((uint32_t)Inverter_RL.ID_AMK_Set + (uint32_t)0x275000, &T_INV_RL_AMK_Set_Point_log,0);
-	setTransmitMessageLog((uint32_t)Inverter_FR.ID_AMK_Set + (uint32_t)0x275000, &T_INV_FR_AMK_Set_Point_log,0);
-	setTransmitMessageLog((uint32_t)Inverter_RR.ID_AMK_Set + (uint32_t)0x275000, &T_INV_RR_AMK_Set_Point_log,0);
+	// setTransmitMessageLog((uint32_t)Inverter_FL.ID_AMK_Set + (uint32_t)0x275000, &T_INV_FL_AMK_Set_Point_log,0);
+	// setTransmitMessageLog((uint32_t)Inverter_RL.ID_AMK_Set + (uint32_t)0x275000, &T_INV_RL_AMK_Set_Point_log,0);
+	// setTransmitMessageLog((uint32_t)Inverter_FR.ID_AMK_Set + (uint32_t)0x275000, &T_INV_FR_AMK_Set_Point_log,0);
+	// setTransmitMessageLog((uint32_t)Inverter_RR.ID_AMK_Set + (uint32_t)0x275000, &T_INV_RR_AMK_Set_Point_log,0);
 	//Init CAN msg of AMK_Actual_Values1
 	setTransmitMessageLog((uint32_t)Inverter_FL.ID_AMK_Ac1 + (uint32_t)0x275000, &T_INV_FL_AMK_Actual_Values1_log,0);
 	setTransmitMessageLog((uint32_t)Inverter_RL.ID_AMK_Ac1 + (uint32_t)0x275000, &T_INV_RL_AMK_Actual_Values1_log,0);
